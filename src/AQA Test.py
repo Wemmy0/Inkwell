e4f2e0ce1dc4dedd7257621729c1c92b2bfd37ac
tom <thomash2505@pm.me>
2024-01-27 14:51:39 +0000

    Remove test files

--- src/AQA Test.py
+++
@@ -1,1 +0,0 @@
-while True: eval(["exit()" if all(i in "QWERTYUIOPASDFGHJKLZXCVBNM" for i in user) and user.upper() == user and len(set(user)) == len(user) and sum([ord(i) for i in user]) >= 420 and sum([ord(i) for i in user]) <= 600 and len(user) >= 5 and len(user) <= 7 else "print ('Not Valid!')" for user in [input("> ")]][0])
